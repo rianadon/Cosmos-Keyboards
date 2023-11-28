abd9a60247c4259a2daf9757aa5efb53ed82483e
Ryan Adolf <ryanadolf123@gmail.com>
2023-11-28 14:46:19 -0500

    Update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 <main class="text-center font-sans bg-slate-900 text-white">
   <div class="background mx-4 relative" style="background-image: url('{base}/keyboard2.png')">
     <h1
-      class="capitalize absolute w-full text-2xl top-[min(calc(20vh-2em),4em)] text-purple font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-amber-600 tracking-tight"
+      class="capitalize absolute w-full text-2xl top-[min(calc(15vh-1.5em),4em)] text-purple font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-amber-600 tracking-tight"
     >
       A new ergonomic keyboard generator
     </h1>
@@ -29,44 +29,53 @@
   >
   <img class="mt-8 h-[4vw]" src="{base}/bg2.png" alt="" />
   <div class="bg-[#003935] p-5">
-    <p class="m-auto font-sans max-w-4xl p-8">
-      Early this year I printed and made my first keyboard, a Dactyl Manuform. I built <a
-        href="https://ryanis.cool/dactyl">an online generator</a
-      > to design more of them too. I've been overjoyed seeing the positive reception it's gotten since
-      its release, but in building the generator I've seen so many opportunities for improvement. Cosmos
-      is my answer to these ideas. A new generator built on a different set of principles, its aim is
-      to let you build the most comfortable and beautiful keyboard and celebrate the joy of making yourself.
-    </p>
-    <div class="md:flex max-w-7xl gap-4 lg:gap-8 m-auto">
-      <VerticalFeature src="/positions.mp4">
-        <span slot="title"
-          >Flexible In <span class="font-bold text-purple">All Dimensions</span></span
-        >
-        <p slot="content">
-          Take control of every key's position. Designing a 3-key macropad? Cosmos will give you a
-          fitting case.
-        </p>
-      </VerticalFeature>
-      <VerticalFeature src="/keyboard.mp4">
-        <span slot="title"
-          >For <span class="font-bold text-emerald">First-Time Pioneers</span> and Seasoned Adventurers</span
-        >
-        <p slot="content">
-          Cosmos will walk you through designing, wiring, and programming your keyboard.
-        </p>
-      </VerticalFeature>
-      <VerticalFeature src="/curves.mp4">
-        <span slot="title">As <span class="font-bold text-lightblue">Smooth</span> As Water</span>
-        <p slot="content">
-          Not only can you replace sharp edges with configurable curves, but the generator runs
-          buttery smooth too.
-        </p>
-      </VerticalFeature>
-    </div>
-    <p class="text-xs text-white/70 my-6">
-      Every frame of the animations above was created with the help of the Cosmos generator. You can
-      make any one of these keyboards yourself.
-    </p>
+    <h2 class="w-full text-2xl top-[min(calc(20vh-2em),4em)] font-semibold mb-4">
+      See It In Action
+    </h2>
+    <video controls muted class="max-w-4xl w-full mx-auto rounded">
+      <source type="video/mp4" src="{base}/cosmos.mp4" />
+    </video>
+    {#if false}
+      <p class="m-auto font-sans max-w-4xl p-8">
+        Early this year I printed and made my first keyboard, a Dactyl Manuform. I built <a
+          href="https://ryanis.cool/dactyl">an online generator</a
+        > to design more of them too. I've been overjoyed seeing the positive reception it's gotten since
+        its release, but in building the generator I've seen so many opportunities for improvement. Cosmos
+        is my answer to these ideas. A new generator built on a different set of principles, its aim
+        is to let you build the most comfortable and beautiful keyboard and celebrate the joy of making
+        yourself.
+      </p>
+      <div class="md:flex max-w-7xl gap-4 lg:gap-8 m-auto">
+        <VerticalFeature src="/positions.mp4">
+          <span slot="title"
+            >Flexible In <span class="font-bold text-purple">All Dimensions</span></span
+          >
+          <p slot="content">
+            Take control of every key's position. Designing a 3-key macropad? Cosmos will give you a
+            fitting case.
+          </p>
+        </VerticalFeature>
+        <VerticalFeature src="/keyboard.mp4">
+          <span slot="title"
+            >For <span class="font-bold text-emerald">First-Time Pioneers</span> and Seasoned Adventurers</span
+          >
+          <p slot="content">
+            Cosmos will walk you through designing, wiring, and programming your keyboard.
+          </p>
+        </VerticalFeature>
+        <VerticalFeature src="/curves.mp4">
+          <span slot="title">As <span class="font-bold text-lightblue">Smooth</span> As Water</span>
+          <p slot="content">
+            Not only can you replace sharp edges with configurable curves, but the generator runs
+            buttery smooth too.
+          </p>
+        </VerticalFeature>
+      </div>
+      <p class="text-xs text-white/70 my-6">
+        Every frame of the animations above was created with the help of the Cosmos generator. You
+        can make any one of these keyboards yourself.
+      </p>
+    {/if}
   </div>
   <img class="h-[4vw]" src="{base}/bg3.png" alt="" />
   <div
@@ -74,31 +83,37 @@
   >
     <p>EC11 Encoders</p>
     <p>Amoeba PCB Holders</p>
+    <p>Microcontroller Holder ¹</p>
     <p>Trackballs</p>
     <p>Choc Switches</p>
     <p>OLED Displays</p>
     <p>Smart Screw Hole Placement</p>
-    <p>20% Less Filament ¹</p>
+    <p>20% Less Filament ²</p>
     <p>Angular Key Spread</p>
-    <p>Adapts to Keycap Tilt ²</p>
+    <p>Adapts to Keycap Tilt ³</p>
     <p>Automatic Model Height</p>
     <p>Uniform Wall Cross Sections</p>
     <p>Thin Surface Detection</p>
     <p>Key Intersection Detection</p>
+    <p>Rounded Edges</p>
     <p>Hand Scanning</p>
+    <p>BOM Generation</p>
     <p>And More!</p>
   </div>
   <div class="mb-18">
     <p class="text-slate-400">
-      ¹ Comparing a 5x5-key model with the <a style="color: unset" href="https://ryanis.cool/dactyl"
+      ¹ Integrated Board Holder supports Pro Micro, RP2040, and nRF5280-based boards.
+    </p>
+    <p class="text-slate-400">
+      ² Comparing a 5x5-key model with the <a style="color: unset" href="https://ryanis.cool/dactyl"
         >Dactyl generator</a
       >.
     </p>
-    <p class="text-slate-400">² Compatible with XDA, DSA, SA, MT3—even OEM and Cherry!</p>
+    <p class="text-slate-400">³ Compatible with XDA, DSA, SA, MT3—even OEM and Cherry!</p>
   </div>
 
   <div class="max-w-5xl m-auto">
-    <HorizontalFeature src="/hand.mp4" alt="Scrren recording of the hand model in Cosmos" reversed>
+    <HorizontalFeature src="/hand.mp4" reversed>
       <span slot="title">A Keyboard for Every Being In the Universe</span>
       <p slot="content">Measure your hand and use it to design the perfect keyboard</p>
       <p slot="description">
@@ -107,7 +122,7 @@
         you to put yourself in the design process.
       </p>
     </HorizontalFeature>
-    <HorizontalFeature src="/cosmos.png" alt="Controls an Code editor in Cosmos">
+    <HorizontalFeature src="/cosmos.png">
       <span slot="title">Intuitive Controls, Everywhere</span>
       <p slot="content">Play with sliders or code, your choice</p>
       <p slot="description">
@@ -117,11 +132,7 @@
         TypeScript to programmatically generate the key layout.
       </p>
     </HorizontalFeature>
-    <HorizontalFeature
-      src="/cosmoboard.png"
-      alt="Selection of keyboards configured in Cosmos"
-      reversed
-    >
+    <HorizontalFeature src="/cosmoboard.png" reversed>
       <span slot="title">The Galaxy is the Limit</span>
       <p slot="content">Design anything, as long as it's a keyboard</p>
       <p slot="description">
@@ -131,7 +142,7 @@
         like the ones on the left in under 5 minutes.
       </p>
     </HorizontalFeature>
-    <HorizontalFeature src="/cad.mp4" alt="Cosmos model viewed in OnShape CAD software">
+    <HorizontalFeature src="/cad.mp4">
       <span slot="title">Compatible With Your 3D Printer And Your CAD Software</span>
       <p slot="content">
         Generate high-quality STLs for printing or STEP files for further editing
@@ -158,20 +169,26 @@
       If you don't mind buggy software, join the <a href={discord}>Discord</a> and try the beta!
     </p>
     <h3 class="font-bold mb-1 mt-8">Is the project Open Source?</h3>
-    <p>I'm keeping the tool closed-source as I work on it.</p>
     <p>
-      I'd like to recoup some of the costs buying keyboard parts, and I'm planning on making the
-      curves generation available for a small fee.
+      Cosmos uses an open-core model. Everything you need to scan your hand to build and program a
+      keyboard is or is going to be open source. Meanwhile some cosmetic features (such as rounded
+      sides) are not open-source and available for a small fee so that I can recoup some of the
+      costs buying keyboard parts.
     </p>
-    <p>Once the project is released, everything else will be open-sourced.</p>
+    <p>
+      So far <a href="https://github.com/rianadon/cosmos-Keyboards/"
+        >the hand-scanning process is open source</a
+      >. I'll be open-sourcing more as the codebase stabilizes.
+    </p>
     <h3 class="font-bold mb-1 mt-8">Can I use the generator today?</h3>
     <p>
       If you're migrating from my <a href="https://ryanis.cool/dactyl">Dactyl generator</a>, you'll
-      find Cosmos already produces more robust models, and I recommend you switch.
+      find Cosmos already produces more robust models, and I recommend you switch, as long as you're
+      ok with a few breaking changes.
     </p>
     <p>
-      New features like trackball support, OLED display, and hand scanning have been released, but I
-      do not find their quality to be suitable.
+      New features like trackball support and OLED displays have been released, but I recommend
+      waiting for Cosmos to mature before using these unless you like hot glue.
     </p>
   </div>
   <p class="text-sm mb-10">
@@ -194,7 +211,7 @@
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
-    height: calc(90vh - 100px);
+    height: calc(80vh - 150px);
     max-height: 600px;
   }
 
