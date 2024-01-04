39048444c7738ea44b567a76622bfdb9a03a7bd3
Ryan Adolf <ryanadolf123@gmail.com>
2024-01-03 17:25:12 -0800

    Add title and favicon for the generator page

--- src/routes/beta/+page.svelte
+++ src/routes/beta/+page.svelte
@@ -1,7 +1,14 @@
 <script>
+  import { base } from '$app/paths'
   import { browser } from '$app/environment'
   import App from './App.svelte'
 </script>
+
+<svelte:head>
+  <title>Cosmos Keyboard Generator</title>
+  <link rel="canonical" href="https://ryanis.cool/cosmos/" />
+  <link rel="icon" href="{base}/favicon.png" />
+</svelte:head>
 
 {#if browser}
   <App />
