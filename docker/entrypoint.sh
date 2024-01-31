88aec3b72892fa0d904b9bd2656db836a308a0fd
Logan Parker <39232041+lopar6@users.noreply.github.com>
2024-01-31 12:38:04 -0700

    Remove .env file requirement, load directly in build and entrypoint (#13)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,6 @@ cp -r /Cosmos-Keyboards/* /temp-cosmos
 cp -r /temp-cosmos/* /Cosmos-Keyboards
 rm -rf /temp-cosmos
 
+if [ -f ./.env ]; then set -a && . ./.env && set +a; fi
 npx concurrently "venv/bin/mkdocs serve -a 0.0.0.0:8000" "npm:dev -- --host 0.0.0.0 --port 5173"
 exec "$@"
