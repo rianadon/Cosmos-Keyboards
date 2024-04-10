4be128701bb411392870e4db2c20c7c94a440ebc
Ryan Adolf <ryanadolf123@gmail.com>
2024-04-10 00:19:08 -0400

    I committed too early

--- src/routes/scan2/store.ts
+++ src/routes/scan2/store.ts
@@ -2,7 +2,7 @@ import { storable } from '$lib/store'
 import { writable } from 'svelte/store'
 import { INITIAL_STAT } from './lib/stats'
 
-export const step = writable(7)
+export const step = writable(0)
 export const mmToPx = storable('mmToPx', 3.78) // 96/25.4, ie how css defines a mm
 export const pc = writable<RTCPeerConnection | null>(null)
 export const remoteStream = writable<{ stream: MediaStream } | null>(null)
