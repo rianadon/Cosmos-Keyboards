041ad802ed83a3d171b57b1b04af878065950958
Marshall Somerville <alakuu32690@gmail.com>
2024-10-27 00:53:47 -0400

    Keyed sockets

--- src/lib/geometry/socketsParts.ts
+++ src/lib/geometry/socketsParts.ts
@@ -196,6 +196,17 @@ export const PART_INFO: Record<CuttleKey['type'], PartInfo> = {
     keycap: true,
     extraBomItems: { ...BOM_DIODE, ...BOM_CHOC_HOTSWAP },
   },
+    'mx-skree': {
+      partName: 'MX-Compatible (Cherry, Gateron, ...)',
+      bomName: 'MX-Compatible Switches',
+      category: 'Sockets',
+      stepFile: '/src/assets/key-mx-skree.step',
+      partOverride: MX_PART,
+      socketSize: [18, 18, 4.7],
+      partBottom: [box(18.7, 18.7,7.5)],
+      keycap: true,
+      extraBomItems: { ...BOM_DIODE },
+  },
   'ec11': {
     partName: 'EC11 Encoder',
     bomName: 'EC11 Encoders',
